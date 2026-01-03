2025158f22089d6d37b250c05a344d3451d60397
startergo <7897244+startergo@users.noreply.github.com>
2026-01-03 11:41:28 -0500

    Update v1.0.3 source tarball sha256 to 5a9f2bace43da0cff72faccf6cc405faa3bcbece95817ada86ae9b25e8e79adc

--- Formula/angle.rb
+++ Formula/angle.rb
@@ -10,7 +10,7 @@ class Angle < Formula
   depends_on "ninja" => :build
   depends_on "llvm" => :build
   url "https://github.com/startergo/homebrew-angle/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "db50ce0e795805739235a8e95361ae1ef2a4d2ddfe412dc8e7aa071419ae8f2d"
+  sha256 "5a9f2bace43da0cff72faccf6cc405faa3bcbece95817ada86ae9b25e8e79adc"
   license "BSD-2-Clause"
 
   head "https://chromium.googlesource.com/angle/angle",
